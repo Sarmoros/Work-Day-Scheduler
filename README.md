e7e8c52bdcfd05b5668dfddfb8b4252a6f158def
Sarahy <sarahym0703@gmail.com>
2023-11-06 22:51:27 -0500

    added readme screenshot of site

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Work-Day-Scheduler
 
 Created a work day scheduler, to have a helpful and organized system for your tasks, plans, or meetings for the day. It also specifically targets the traditional work day hours, 9am to 5pm. With this simple site there are rows, each row dedicated to one hour, with the time it is for, a text box for you to type, and a save button. In the text area you can type whatever you please, whatever you want to dedicate this hour of your work day for, then follow by a blue button, the save button. With the save button it will save into your local storage, now the events planned for the day will remain on your personal calendar site to come back to whenever.
+
+![Website](./assets/images/Screenshot(6).png "Screenshot of page")
--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,13 @@
 $(document).ready(function() {
 
   $(".saveBtn").on("click", function() {
-    // Use the id in the containing time-block as a key to save the user input in local storage
+    // Uses the id in the containing time-block as a key to save the user input in local storage
     var blockId = $(this).parent().attr("id");
     var userInput = $(this).siblings(".description").val();
     localStorage.setItem(blockId, userInput);
   });
 
-  // Add code to apply the past, present, or future class to each time block
+  // Added code to apply the past, present, or future class to each time block
   var currentHour = dayjs().hour();
   $(".time-block").each(function() {
     var blockHour = parseInt($(this).attr("id").split("-")[1]);
@@ -21,7 +21,7 @@ $(document).ready(function() {
     }
   });
 
-  // Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements
+  // Added code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements
   $(".time-block").each(function() {
     var blockId = $(this).attr("id");
     var savedInput = localStorage.getItem(blockId);
@@ -31,7 +31,7 @@ $(document).ready(function() {
     }
   });
 
-  // Add code to display the current date in the header of the page
+  // Added code to display the current date in the header of the page
   var currentDate = dayjs().format("dddd, MMMM D, YYYY");
   $("#currentDay").text(currentDate);
 
